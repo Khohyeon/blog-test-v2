a8e389827275d24df148bccf3042287dec4c80d0
Khohyeon <122351733+Khohyeon@users.noreply.github.com>
2023-02-10 01:08:52 +0900

    회원가입 생성! (#5)
    * 회원가입 Service 세팅완료
    
    * 회원가입 Controller 완료
    
    * 회원가입  test 완료

--- src/main/java/shop/mtcoding/blog1/controller/user/UserController.java
+++ src/main/java/shop/mtcoding/blog1/controller/user/UserController.java
@@ -1,19 +1,36 @@
 package shop.mtcoding.blog1.controller.user;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import shop.mtcoding.blog1.dto.user.UserReq.JoinReqDto;
+import shop.mtcoding.blog1.model.UserRepository;
+import shop.mtcoding.blog1.service.UserService;
+
 @Controller
 public class UserController {
 
-    @GetMapping("joinForm")
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private UserService userService;
+
+    @GetMapping("/joinForm")
     public String joinForm() {
         return "user/joinForm";
     }
 
-    @PostMapping("join")
-    public String join() {
-        return "";
+    @PostMapping("/join")
+    public String join(JoinReqDto joinReqDto) {
+        userService.회원가입(joinReqDto);
+        return "redirect:/loginForm";
+    }
+
+    @GetMapping("/loginForm")
+    public String loginForm() {
+        return "user/loginForm";
     }
 }
--- src/main/java/shop/mtcoding/blog1/model/UserRepository.java
+++ src/main/java/shop/mtcoding/blog1/model/UserRepository.java
@@ -7,17 +7,21 @@ import org.apache.ibatis.annotations.Param;
 
 @Mapper
 public interface UserRepository {
-    public int insert(@Param("username") String username, @Param("password") String password,
-            @Param("email") String email);
+        public int insert(@Param("username") String username, @Param("password") String password,
+                        @Param("email") String email);
 
-    public int updateById(@Param("id") int id, @Param("username") String username,
-            @Param("password") String password,
-            @Param("email") String email);
+        public int updateById(@Param("id") int id, @Param("username") String username,
+                        @Param("password") String password,
+                        @Param("email") String email);
 
-    public int deleteById(int id);
+        public int deleteById(int id);
 
-    public List<User> findAll();
+        public List<User> findAll();
 
-    public User findById(int id);
+        public User findById(int id);
+
+        public User findByUsernameAndPassword(@Param("username") String username, @Param("password") String password);
+
+        public User findByUsername(@Param("username") String username);
 
 }
