a786a912a516e1bf7f6a99e0dfc4fab370718b66
kim ho hyeon <khh5762@naver.com>
2023-02-10 00:53:45 +0900

    회원가입 Service 세팅완료

--- src/main/java/shop/mtcoding/blog1/controller/user/UserController.java
+++ src/main/java/shop/mtcoding/blog1/controller/user/UserController.java
@@ -1,11 +1,22 @@
 package shop.mtcoding.blog1.controller.user;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import shop.mtcoding.blog1.dto.user.UserReq.JoinReqDto;
+import shop.mtcoding.blog1.model.UserRepository;
+import shop.mtcoding.blog1.service.UserService;
+
 @Controller
 public class UserController {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private UserService userService;
 
     @GetMapping("joinForm")
     public String joinForm() {
@@ -13,7 +24,8 @@ public class UserController {
     }
 
     @PostMapping("join")
-    public String join() {
+    public String join(JoinReqDto joinReqDto) {
+        userService.회원가입(joinReqDto);
         return "";
     }
 }
