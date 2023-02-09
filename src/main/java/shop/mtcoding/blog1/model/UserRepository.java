15e749308197a39b686a2e4f09e361538153484b
kim ho hyeon <khh5762@naver.com>
2023-02-10 00:14:52 +0900

    user.xml 세팅 완료

--- src/main/java/shop/mtcoding/blog1/model/UserRepository.java
+++ src/main/java/shop/mtcoding/blog1/model/UserRepository.java
@@ -1,5 +1,23 @@
 package shop.mtcoding.blog1.model;
 
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+@Mapper
 public interface UserRepository {
+    public int insert(@Param("username") String username, @Param("password") String password,
+            @Param("email") String email);
+
+    public int updateById(@Param("id") int id, @Param("username") String username,
+            @Param("password") String password,
+            @Param("email") String email);
+
+    public int deleteById(int id);
+
+    public List<User> findAll();
+
+    public User findById(int id);
 
 }
