ed0db24d9086d5b1cb26fbda21196a7b2267cc84
kim ho hyeon <khh5762@naver.com>
2023-02-10 00:13:26 +0900

    model(user,board) 세팅 완료

--- src/main/java/shop/mtcoding/blog1/controller/user/UserController.java
+++ src/main/java/shop/mtcoding/blog1/controller/user/UserController.java
@@ -2,6 +2,7 @@ package shop.mtcoding.blog1.controller.user;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class UserController {
@@ -9,5 +10,10 @@ public class UserController {
     @GetMapping("joinForm")
     public String joinForm() {
         return "user/joinForm";
+    }
+
+    @PostMapping("join")
+    public String join() {
+        return "";
     }
 }
---
+++ src/main/java/shop/mtcoding/blog1/model/User.java
@@ -0,0 +1,5 @@
+package shop.mtcoding.blog1.model;
+
+public class User {
+
+}
