a1fbe6a522ba89e80d6ec3f8f9dc0540eb863e58
kim ho hyeon <khh5762@naver.com>
2023-02-11 14:52:57 +0900

    ajax통신 => CustomApiException 세팅완료

--- src/main/java/shop/mtcoding/blog1/handler/CustomExceptionHanlder.java
+++ src/main/java/shop/mtcoding/blog1/handler/CustomExceptionHanlder.java
@@ -4,6 +4,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import shop.mtcoding.blog1.dto.ResponseDto;
+import shop.mtcoding.blog1.handler.ex.CustomApiException;
 import shop.mtcoding.blog1.handler.ex.CustomException;
 import shop.mtcoding.blog1.util.Script;
 
@@ -16,4 +18,10 @@ public class CustomExceptionHanlder {
         String responseBody = Script.back(e.getMessage());
         return new ResponseEntity<>(responseBody, e.getStatus());
     }
+
+    @ExceptionHandler(CustomApiException.class)
+    public ResponseEntity<?> customApiException(CustomApiException e) {
+        return new ResponseEntity<>(new ResponseDto<>(-1, e.getMessage(), null), e.getStatus());
+    }
+
 }
