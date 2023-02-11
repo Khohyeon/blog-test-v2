8b081e24018bf047950c70e21d21267ec9e78e91
Khohyeon <122351733+Khohyeon@users.noreply.github.com>
2023-02-11 15:31:12 +0900

    메인 목록 보기 완료 (#8)
    * 메인 목록 Dto 생성
    
    * findAllWithUser (User join Board) 생성
    
    * BoardRepository -> findAllWithUser 생성
    
    * 메인 목록 보기 완료!

--- src/main/java/shop/mtcoding/blog1/dto/board/BoardResp.java
+++ src/main/java/shop/mtcoding/blog1/dto/board/BoardResp.java
@@ -1,5 +1,16 @@
 package shop.mtcoding.blog1.dto.board;
 
+import lombok.Getter;
+import lombok.Setter;
+
 public class BoardResp {
 
+    @Setter
+    @Getter
+    public static class BoardMainRespDto {
+        private int id;
+        private String title;
+        private String thumbnail;
+        private String username;
+    }
 }
